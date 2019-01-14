961686780621aa59f18a760c95805441727321fa
Manish Singh <manishfartyal9090@gmail.com>
2019-01-14 18:12:44 +0530

    article

--- tests/user_service.py
+++ tests/user_service.py
@@ -12,10 +12,10 @@ def insert_quote(title,quote,user_id):
 	doc=table1.find_one({'_id':user_id})
 	if doc.get('Articles',None) is None:
 		print(doc)
-		table1.update_one({'_id':user_id},{'$push':{'Articles':[{'article_id':1,'title':title,'quote':quote}]}})
+		table1.update_one({'_id':user_id},{'$push':{'Articles':{'article_id':1,'title':title,'quote':quote}}})
 	else:
 		length =len(doc["Articles"])
 		length +=1
-		table1.update_one({'_id':user_id},{'$push':{'Articles':[{'article_id':length,'title':title,'quote':quote}]}})
+		table1.update_one({'_id':user_id},{'$push':{'Articles':{'article_id':length,'title':title,'quote':quote}}})
 
 
